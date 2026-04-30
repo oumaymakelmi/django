899d821503a69e2151f0b9c8e81377ce9b29ffa4
Rania jawedi <votre.email@raniajawedi67@gmail.com>
2026-04-30 15:26:36 +0200

    valide

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,8 +1,8 @@
 
 #print("khalils")#afichage de khalils
 =======
-print("hello")
-nom = input("entrer un nom")
-prenom = input("entrer un prenom")
-print("bonjour " + nom + " " + prenom)
+print("good morning")
+nom = input("your name")
+prenom = input("your last name")
+print("good morning " + nom + " " + prenom)
 >>>>>>> 52067e76c2476efb8c1ec7a04e2dbd17a3b9d502
